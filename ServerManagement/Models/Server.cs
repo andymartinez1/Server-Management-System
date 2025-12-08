0dd3a1ab95b05d83111851f30bf0fb3a491ac9d0
Andrew Martinez <martinez.s.andrew@gmail.com>
2025-12-07 19:10:57 -0500

    Added a server model and repository class. Added a city repository. Also added new routable components with route parameters and constraints.

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,19 @@
+﻿namespace ServerManagement.Models;
+
+public class Server
+{
+    public Server()
+    {
+        var random = new Random();
+        var randomNumber = random.Next(0, 2);
+        IsOnline = randomNumber == 0 ? false : true;
+    }
+
+    public int ServerId { get; set; }
+
+    public bool IsOnline { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string City { get; set; } = string.Empty;
+}
---
+++ ServerManagement/Repository/CityRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace ServerManagement.Repository;
+
+public class CityRepository
+{
+    private static readonly List<string> cities = new()
+    {
+        "Toronto",
+        "Montreal",
+        "Ottawa",
+        "Calgary",
+        "Halifax"
+    };
+
+    public static List<string> GetCities()
+    {
+        return cities;
+    }
+}
